5678b2c7e6fddf947d4b79824b5c8ac9da2cb7b4
つ 空白 <oncwnuIr_jTZqt9Tye4CGjYf0VCU@git.weixin.qq.com>
2026-04-08 01:04:50 +0800

    构建: 更新版本号为1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.pulse.checkin"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
